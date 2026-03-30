3e8a0b501f355f94f572fc2342bcfb54e67bce52
E Sai Suhas Chandra <enjamurisaisuhas@gmail.com>
2026-03-31 02:05:57 +0530

    ocr_reader.py

--- ocr_reader.py
+++ ocr_reader.py
@@ -1,42 +1,28 @@
 import easyocr
 import os
-import platform
-from pdf2image import convert_from_path
 
-#  POPPLER SETUP 
-if platform.system() == "Windows":
-    POPPLER_PATH = r"C:\Users\Admin\Downloads\poppler-25.12.0\Library\bin"
-else:
-    POPPLER_PATH = None
-
-reader = easyocr.Reader(['en'], gpu=False)
+reader = easyocr.Reader(['en'])
 
 TEXT_FOLDER = "extracted_text"
 os.makedirs(TEXT_FOLDER, exist_ok=True)
 
-def pdf_to_images(file_path):
-    if POPPLER_PATH:
-        return convert_from_path(file_path, poppler_path=POPPLER_PATH)
-    else:
-        return convert_from_path(file_path)
 
-def extract_text(file_path, doc_type):
+def extract_text(image_path, doc_type):
+
+    print("Running EasyOCR for:", doc_type)
+
+    results = reader.readtext(image_path)
+
     text = ""
 
-    if file_path.lower().endswith(".pdf"):
-        images = pdf_to_images(file_path)
-        for img in images:
-            results = reader.readtext(img, detail=0)
-            for txt in results:
-                text += txt + "\n"
-    else:
-        results = reader.readtext(file_path, detail=0)
-        for txt in results:
-            text += txt + "\n"
+    for (_, txt, _) in results:
+        text += txt + "\n"
 
     save_path = os.path.join(TEXT_FOLDER, f"{doc_type}.txt")
 
     with open(save_path, "w", encoding="utf-8") as f:
         f.write(text)
+
+    print("Saved:", save_path)
 
     return text
